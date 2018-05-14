152168a0a0056089e3a09f3c219eec9072580c4e
elharony <p.yahyaelharony@gmail.com>
2018-05-14 14:32:50 +0200

    Write an Async method to the 'AddressBook' class

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,6 +1,7 @@
 class AddressBook {
     constructor() {
         this.contacts = [];
+        this.initialComplete = false;
     }
     addContact(contact) {
         this.contacts.push(contact);
@@ -10,5 +11,15 @@ class AddressBook {
     }
     deleteContact(index) {
         this.contacts.splice(index, 1);
+    }
+    getInitialContacts(cb) {
+        let self = this;
+
+        setTimeout(function() {
+            self.initialComplete = true;
+            if (cb) {
+                return cb();
+            }
+        }, 3);
     }
 }
